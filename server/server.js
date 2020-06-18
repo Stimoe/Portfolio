5ff9a2f7efb522c555e98c65d72b884f85be9d29
Stimoe <stimoe3@gmail.com>
2020-06-18 11:55:49 -0700

    test server

--- server/server.js
+++ server/server.js
@@ -10,3 +10,28 @@
 // app.listen(port, () => {
 //   console.log("Server is up!");
 // });
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+
+//     const path = require('path');
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+
+//   }
+
+const express = require("express");
+const app = express();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
